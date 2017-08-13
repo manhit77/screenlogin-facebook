ee3f13728528f2f818ad2d4762b8f5769d7a71c3
manhit77 <vmanhit@gmail.com>
2017-08-13 17:37:08 +0700

    uploadfile

---
+++ index.android.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import Index from './app/index.js';
+AppRegistry.registerComponent('LoginFacebook', () => Index);
